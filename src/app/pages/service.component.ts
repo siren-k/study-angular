951f86e7a9795ce3141009c1523bd5ec6096cdcb
Siren-K <siren.k.pro@gmail.com>
2021-05-09 21:19:38 +0900

    add base routing feature

---
+++ src/app/pages/service.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service',
+  template: `
+    <p>
+      service works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
